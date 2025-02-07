ec1389cceac2c2da4e39ad9f5cee27fdc639b633
Rayner Losque <rayner.losque@e-auditoria.com.br>
2025-02-07 12:57:54 -0300

    subindo ajuste

--- cypress/e2e/primeiro_teste.cy.js
+++ cypress/e2e/primeiro_teste.cy.js
@@ -1,9 +1,9 @@
 describe('Teste da Página Inicial', () => {
   it('Visita a página inicial', () => {
-    cy.visit('https://losqui.github.io/cypress-ai-generator/'); // Substitua pela URL da sua Página
+    cy.visit('cadastro.html'); // Substitua pela URL da sua página
 
     // Exemplo simples: verifica se o título da página contém a palavra 'Chatbot'
-    cy.get('header').should('contain', 'Cadastro de Usuário'); // Adapte o texto esperado
+    cy.get('header').should('contain', 'Cadastro de Usuários'); // Adapte o texto esperado
     
   });
 });
