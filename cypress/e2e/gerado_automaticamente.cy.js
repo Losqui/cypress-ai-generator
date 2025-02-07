903c621230c2c786dd1e36193abc08d3f34af238
Rayner Losque <rayner.losque@e-auditoria.com.br>
2025-02-07 17:32:40 -0300

    Removido botão Cancelar e gerado outro automaticamente

--- cypress/e2e/gerado_automaticamente.cy.js
+++ cypress/e2e/gerado_automaticamente.cy.js
@@ -32,31 +32,4 @@ describe('Teste Gerado Automaticamente', () => {
 
     });
   
-
-    it('Clica no botão "Cancelar"', () => {
-      cy.visit('https://losqui.github.io/cypress-ai-generator/'); // Substitua pela URL da sua página
-      cy.contains('button', 'Cancelar').click();
-      // Adicione asserções aqui para verificar o resultado do clique
-       cy.get('#cadastro-form > div:nth-child(1)').within(() => {
-              cy.get('.ant-form-item-explain-error').should('contain', 'Por favor, insira o Nome.');
-          });
-
-          cy.get('#cadastro-form > div:nth-child(2)').within(() => {
-              cy.get('.ant-form-item-explain-error').should('contain', 'Por favor, insira o Email.');
-          });
-
-          cy.get('#cadastro-form > div:nth-child(3)').within(() => {
-              cy.get('.ant-form-item-explain-error').should('contain', 'Por favor, insira a Senha.');
-          });
-
-          cy.get('#cadastro-form > div:nth-child(4)').within(() => {
-              cy.get('.ant-form-item-explain-error').should('contain', 'Por favor, selecione o UF.');
-          });
-
-          cy.get('#cadastro-form > div:nth-child(5)').within(() => {
-              cy.get('.ant-form-item-explain-error').should('contain', 'Por favor, selecione o Sexo.');
-          });
-
-    });
-  
 });
