697df6e4a07a8cf28ee195213a75eb749ba74c64
Rayner Losque <rayner.losque@e-auditoria.com.br>
2025-02-07 10:43:08 -0300

    Commit 2

--- cypress/e2e/primeiro_teste.cy.js
+++ cypress/e2e/primeiro_teste.cy.js
@@ -1,6 +1,6 @@
 describe('Teste da Página Inicial', () => {
   it('Visita a página inicial', () => {
-    cy.visit('cadastro.html'); // Substitua pela URL da sua página
+    cy.visit('cadastro.html'); // Substitua pela URL da sua Página
 
     // Exemplo simples: verifica se o título da página contém a palavra 'Chatbot'
     cy.get('header').should('contain', 'Cadastro de Usuário'); // Adapte o texto esperado
